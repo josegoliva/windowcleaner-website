4e1eea082179571f827fd3fec6e5020c1c578fe3
Max Kohler <max@koehler-kn.de>
2020-06-27 15:00:54 +0100

    Iterate event view

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ import { gra, gri } from './util.js';
 window.addEventListener('DOMContentLoaded', () => {
     const referenceList = document.querySelector(".references-container")
     const logo = document.querySelector(".site-logo")
-    logo.style.left = `${gri(0, 50)}%`
+    logo.style.left = `${gri(0, 20)}%`
     logo.style.top = `${gri(0, 80)}%`
     const options = {
 
